88753de94965603faac9a5985e4f54b74a7b7667
Jason Dalton <jason@veracis.net>
2020-09-14 11:39:50 -0400

    Update extractSurfacePoints2.py

--- extractSurfacePoints2.py
+++ extractSurfacePoints2.py
@@ -12,7 +12,7 @@ import elevation
 
 
 # clip the SRTM1 30m DEM of Rome and save it to Rome-DEM.tif
-elevation.clip(bounds=(12.35, 41.8, 12.65, 42), output='Rome-DEM.tif')
+#elevation.clip(bounds=(12.35, 41.8, 12.65, 42), output='Rome-DEM.tif')
 elevation.clip(bounds=(-78.9229568629999960,38.9524045909233294, -78.8717227540000039,39.0085208110766644), output='TroutPass-DEM.tif')
 # clean up stale temporary files and fix the cache in the event of a server error
 elevation.clean()
